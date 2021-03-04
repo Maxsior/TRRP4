a5627dcdf9b075efe6d71160f65417cedd261813
Maksim Kudryavtsev <m.kudryavtsev@xsolla.com>
2021-03-04 22:31:36 +0500

    [client] Create skeleton of client

---
+++ client/main.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import click
+from mimetypes import guess_type
+
+HOST = os.getenv('SERVER_HOST') or 'http://localhost:3000'
+
+
+@click.command()
+@click.option('--file', '--image', '-f', required=True, prompt=True, help="Image to upload")
+def upload(file):
+    mime = guess_type(file)[0] or ''
+    if not mime.startswith('image'):
+        click.echo(f'File {file} is not an image')
+        return
+
+    try:
+        with open(file, 'rb') as file:
+            click.echo('uploading...')
+            r = requests.post(HOST, files={'image': file})
+            print(r.raw())  # TODO что возвращается?
+    except OSError:
+        click.echo('File does not exist or not accessible')
+
+
+if __name__ == '__main__':
+    upload()
